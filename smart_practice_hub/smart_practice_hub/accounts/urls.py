33ece87c57d23a9b8f720246cdfa699f613b43d1
LAB23_USER ON WS19 <L23X15W19@LabHCI.net>
2025-10-20 17:08:43 +0800

    fix: project cleanup

--- smart_practice_hub/smart_practice_hub/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('signup/', views.CustomSignUpView.as_view(), name='signup'),
-]
